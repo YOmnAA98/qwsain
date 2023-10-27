31a09fc8b05f5cc59165a3b614eabf05b34a9b99
YOmnAA98 <58415363+YOmnAA98@users.noreply.github.com>
2023-10-27 03:25:04 +0200

    copy done

--- src/app/Pages/course-tutorial/course-tutorial.component.ts
+++ src/app/Pages/course-tutorial/course-tutorial.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+import { Toast } from 'bootstrap';
 
 @Component({
   selector: 'app-course-tutorial',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class CourseTutorialComponent {
 
+  // toast function
+  name = 'Angular ' + VERSION.major;
+  @ViewChild('myToast', { static: true }) toastEl!: ElementRef<HTMLDivElement>;
+  toast: Toast | null = null;
+
+  ngOnInit() {
+    this.toast = new Toast(this.toastEl.nativeElement, {});
+  }
+
+  show() {
+    this.toast!.show();
+  }
+
+  // copy code function
+  code = `<!doctype html>
+  <html>
+    <head>
+
+    </head>
+    <body>
+    
+    </body>
+  </html>`;
+
+  copyCode() {
+    const codeCopy = document.createElement('textarea');
+    codeCopy.value = this.code;
+    document.body.appendChild(codeCopy);
+    codeCopy.focus();
+    codeCopy.select();
+    document.execCommand('copy');
+    document.body.removeChild(codeCopy);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CodeInputModule } from 'angular-code-input';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -58,9 +59,17 @@ import { PaginationComponent } from './Pages/Shared/pagination/pagination.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CodeInputModule
+    CodeInputModule,
+    HighlightModule,  
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js')      
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
